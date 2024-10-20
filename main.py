3564794878e018e28f692590240e2e014b88d744
rahul0715 <ajaychouhan26367@gmail.com>
2024-10-20 11:31:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ import datetime
 import aiohttp
 
 bot = Client("bot",
-             bot_token= "6556042495:AAEYgNL0EDhqpA7P6SLJ-dzFath0q7gzttA", 
+             bot_token= "7063024370:AAFHuTYE6bOwjI5s3Nodv2mi8JGctq15nu0", 
              #bot_token= os.environ.get("BOT_TOKEN"),
-             api_id= 28590119,
-             api_hash= "2494557bf21e6c5152f26070aa1a97c7")
-auth_users = [1923922961,6200710535,5753557653,6404553499]
+             api_id= 27862677,
+             api_hash= "e343ce2c81b2b6c2c0d6bee58284e3bd")
+auth_users = [5881684718,6200710535,5753557653,6404553499]
 #romeo  -1923922961 
 
-owner_id = 1923922961
+owner_id = 5881684718
 # Extras 
 failed_links = []  # List to store failed links
 fail_cap =f"**➜ This file Contain Failed Downloads while Downloding \n You Can Retry them one more time **"
